394884c00e3fbb61cac189560990e0e875d357a6
JECWorks <36215363+JECWorks@users.noreply.github.com>
2024-10-13 21:39:03 -0400

    Explicit animations

--- AnimationsIntro/ContentView.swift
+++ AnimationsIntro/ContentView.swift
@@ -8,49 +8,18 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var animationAmount = 1.0
+    @State private var animationAmount = 0.0
     
     var body: some View {
-//        Button("Tap Me") {
-////            animationAmount += 1
-//        }
-//        .padding(50)
-//        .background(.red)
-//        .foregroundColor(.white)
-//        .clipShape(.circle)
-//        .overlay(
-//            Circle()
-//                .stroke(.red)
-//                .scaleEffect(animationAmount)
-//                .opacity(2-animationAmount)
-//                .animation(
-//                    .easeInOut(duration: 0.75)
-//                    .repeatForever(autoreverses: false),
-//                    value: animationAmount
-//                )
-//            )
-//        .onAppear {
-//            animationAmount = 2
-//        }
-        print(animationAmount)
-        
-        return VStack {
-            Stepper("Scale amount", value: $animationAmount.animation(
-                .easeInOut(duration: 0.75)
-                .repeatCount(3, autoreverses: true)
-            ), in: 1...10)
-            
-            Spacer()
-            
-            Button("Tap me") {
-                animationAmount += 1
-            }
-            .padding(40)
-            .background(.red)
-            .foregroundColor(.white)
-            .clipShape(.circle)
-            .scaleEffect(animationAmount)
-        }
+        Button("Tap me") {
+            withAnimation(.spring(duration: 1, bounce: 0.5)) {
+                animationAmount += 360
+            }        }
+        .padding(50)
+        .background(.red)
+        .foregroundStyle(.white)
+        .clipShape(.circle)
+        .rotation3DEffect(.degrees(animationAmount), axis: (x: 0, y: 1, z: 0))
     }
         
 }
